3319c23295450abda7806a64a2c6adf1620856d3
Abdallah9858 <121675703+Abdallah9858@users.noreply.github.com>
2025-06-10 16:28:26 +0300

    Update and rename start.sh to startuat.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start all scripts in the background
-python3 /app/firewallprod.py >> /app/logs/firewall.log 2>&1 &
-python3 /app/qmpprod.py >> /app/logs/qmp.log 2>&1 &
-# Wait for all background processes to finish
-wait
-
